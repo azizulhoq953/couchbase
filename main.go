5c4fe80a79f5d4b45144fb6fc5bbef200b304a31
azizul_hoq <azizulhoq4305@gamil.com>
2021-03-25 15:51:53 +0600

    couchbase mcb package testing

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"os"
+
+	"github.com/mateors/mcb"
+)
+
+var db *mcb.DB
+
+type RequestTable struct {
+	ID      string `json:"aid"`
+	Name    string `json:"name"`
+	Company string `json:"company"`
+	Email   string `json:"email"`
+	Type    string `json:"type"`
+	Status  int    `json:"status"`
+}
+
+func init() {
+
+	db = mcb.Connect("localhost", "Azizul", "4305Z15")
+
+	res, err := db.Ping()
+	if err != nil {
+
+		fmt.Println(res)
+		os.Exit(1)
+	}
+	fmt.Println(res, err)
+
+}
+
+func main() {
+
+	//How to insert into couchbase bucket
+	var myData RequestTable
+
+	form := make(url.Values, 0)
+	form.Add("bucket", "master_academy") //bucket Name
+	form.Add("aid", "d006")              //document ID
+	form.Add("name", "Azizul")
+	form.Add("company", "master_academy")
+	form.Add("email", "azizulhoq4305@gmail.com")
+	form.Add("type", "request")
+	form.Add("status", "1")
+
+	p := db.Insert(form, &myData)    //pass by reference (&myData)
+	fmt.Println("Status:", p.Status) //p.Status == Success means data successfully inserted to bucket.
+
+	//How to retrieve from couchbase bucket (selected fields only)
+
+	//pres := db.Query("SELECT aid,name,age,profession FROM master_erp WHERE type='participant'")
+	//rows := pres.GetRows()
+
+	//fmt.Println("Total Rows:", len(rows))
+	//fmt.Println(rows)
+
+	//How to retrieve from couchbase bucket (All fields using *)
+
+	//fmt.Println("Total Rows:", len(rows))
+	//fmt.Println(rows)
+
+}
